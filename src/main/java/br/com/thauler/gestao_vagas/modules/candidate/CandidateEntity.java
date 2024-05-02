5798826ef723f0009d2272deb7818830e88f335c
Thauler <thauler98@gmail.com>
2024-05-02 14:33:46 -0300

    feat(candidate): add the candidate controller and entity

--- src/main/java/br/com/thauler/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/thauler/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,2 +1,26 @@
-package br.com.thauler.gestao_vagas.modules.candidate.controllers;public class CandidateEntity {
+package br.com.thauler.gestao_vagas.modules.candidate;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import java.util.UUID;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class CandidateEntity {
+
+  private UUID id;
+  private String name;
+
+  @Pattern(regexp = "^(?!\\s*$).+", message = "is not allowed blank spaces on [username]")
+  private String username;
+
+  @Email(message = "It must be a valid email")
+  private String email;
+
+  @Length(min = 10, max = 100)
+  private String password;
+  private String description;
+  private String curriculum;
 }
--- src/test/java/br/com/thauler/gestao_vagas/GestaoVagasApplicationTests.java
+++ src/test/java/br/com/thauler/gestao_vagas/GestaoVagasApplicationTests.java
@@ -6,8 +6,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GestaoVagasApplicationTests {
 
+<<<<<<< HEAD
 	@Test
 	void contextLoads() {
 	}
+=======
+  @Test
+  void contextLoads() {
+  }
+>>>>>>> 5cf352f (feat(candidate): add the candidate controller and entity)
 
 }
