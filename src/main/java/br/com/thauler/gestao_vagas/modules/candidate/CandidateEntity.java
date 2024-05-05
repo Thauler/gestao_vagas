a9d6d5a7481ee8d11361ae4a376aca67618dbed8
Thauler <thauler98@gmail.com>
2024-05-05 16:48:27 -0300

    feat(exceptions): add validation exceptions

--- src/main/java/br/com/thauler/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/thauler/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,7 @@ package br.com.thauler.gestao_vagas.modules.candidate;
 
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import java.util.UUID;
 import lombok.Data;
@@ -13,13 +14,15 @@ public class CandidateEntity {
   private UUID id;
   private String name;
 
-  @Pattern(regexp = "^(?!\\s*$).+", message = "is not allowed blank spaces on [username]")
+  @NotBlank()
+  @Pattern(regexp = "\\S+", message = "is not allowed blank spaces on [username]")
   private String username;
 
   @Email(message = "It must be a valid email")
   private String email;
 
-  @Length(min = 10, max = 100)
+  @NotBlank()
+  @Length(min = 8, max = 100)
   private String password;
   private String description;
   private String curriculum;
