c2f76d63995f0c1a35be77f81742bf41cf231c8b
Thauler <thauler98@gmail.com>
2024-05-05 20:14:21 -0300

    feat(candidate repository): add findByEmailOrUsername method
    This method will prevent to save a candidate that has the same username or email

--- src/main/java/br/com/thauler/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/thauler/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.thauler.gestao_vagas.modules.candidate.controllers;
 
+import br.com.thauler.gestao_vagas.exceptions.UserFoundException;
 import br.com.thauler.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.thauler.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
@@ -18,6 +19,10 @@ public class CandidateController {
 
   @PostMapping()
   public CandidateEntity create(@Valid @RequestBody CandidateEntity candidate) {
+    this.candidateRepository
+        .findByEmailOrUsername(candidate.getEmail(), candidate.getUsername())
+        .ifPresent((user) -> {throw new UserFoundException();
+        });
     return this.candidateRepository.save(candidate);
   }
 }
